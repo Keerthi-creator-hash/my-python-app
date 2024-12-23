ebe085c80b5f53c4028f1e79f199702953409ce3
Keerthi-creator-hash <keerthi@stavir.com>
2024-12-23 13:53:37 +0530

    Create delete_existing_files.sh

---
+++ scripts/delete_existing_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -f /home/ubuntu/my-python-app/scripts/install_dependencies.sh
+rm -f /home/ubuntu/my-python-app/scripts/install_composer_dependencies.sh
+rm -f /home/ubuntu/my-python-app/scripts/start_server.sh
+rm -f /home/ubuntu/my-python-app/scripts/stop_server.sh
