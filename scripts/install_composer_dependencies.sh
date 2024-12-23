924f1a210960116c5922e595c01d9697045ad71b
Keerthi-creator-hash <keerthi@stavir.com>
2024-12-23 14:21:32 +0530

    Update install_composer_dependencies.sh

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-# Navigate to the application directory and install Python dependencies
+# Navigate to the application directory
 cd /home/ubuntu/my-python-app
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the required packages
 pip install flask==2.3.2
 
+# Deactivate the virtual environment
+deactivate
