c31c028bab4dd82b9134c368e93db5f23bf576f0
Keerthi-creator-hash <keerthi@stavir.com>
2024-12-23 10:07:31 +0530

    some changes

---
+++ scripts/install_composer_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Navigate to the application directory and install Python dependencies
+cd /home/ubuntu/
+pip3 install -r requirements.txt
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Navigate to the application directory and start the Flask application
-cd /var/www/html
+cd /home/ubuntu/
 nohup python3 app.py > app.log 2>&1 &
